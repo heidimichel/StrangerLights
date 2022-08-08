e93425bc250361c2cf03082b21fc5db0998210e0
Heidi Michel <heidi_michel@hotmail.com>
2022-08-08 18:46:29 +0200

    deleted comments and added conditions to remove 2 bugs
    bug #1 was the static lights when someone presses "on" twice in a row. bug #2 was the blinking of turned off lights when someone presses "off" twice in a row.

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function lightsOn() {
-    let lamps = [];
-    lamps = document.querySelectorAll(".lamp");
-    const colorList = ["#FFFF7F" , "#FFC04C", "#FF6666", "#4CFF4C", "#66C5FF"];
-    let i = 0;
-    lamps.forEach((item) => {
+  let lamps = [];
+  lamps = document.querySelectorAll(".lamp");
+  const colorList = ["#FFFF7F" , "#FFC04C", "#FF6666", "#4CFF4C", "#66C5FF"];
+  let i = 0;
+  lamps.forEach((item) => {
+    if (item.classList.contains("remove-animation")) {
       item.style.animationPlayState = 'running';
-      //item.style.setProperty('animation', 'initial');
-      item.classList.toggle("remove-animation");
-      item.style.setProperty('background-color', colorList[i]);
-      item.style.setProperty('box-shadow', `2px 10px 20px ${colorList[i]}`);
-      //item.style.setProperty('animation', );
-      if (i >= 4) {
+    item.classList.toggle("remove-animation");
+    item.style.setProperty('background-color', colorList[i]);
+    item.style.setProperty('box-shadow', `2px 10px 20px ${colorList[i]}`);
+    if (i >= 4) {
         i = 0;
       } else {
         i++;
-      }    
-    });
-    console.log("it is on!");
-  };
-  
-  function lightsOff() {
-    let lamps = [];
-    lamps = document.querySelectorAll(".lamp");
-    lamps.forEach((item) => {
-      item.style.setProperty('background-color', '#362211');
-      item.style.setProperty('box-shadow', 'none');
-      //item.style.setProperty('animation', 'none');
-      item.classList.toggle("remove-animation");
-      //item.style.animationPlayState = 'paused';   
-    });
-    console.log("it is off!");
-  };
+      }
+    };
+  });
+  console.log("it is on!"); 
+};
+
+function lightsOff() {
+  let lamps = [];
+  lamps = document.querySelectorAll(".lamp");
+  lamps.forEach((item) => {
+    if (!item.classList.contains("remove-animation")) {
+     item.style.setProperty('background-color', '#362211');
+     item.style.setProperty('box-shadow', 'none');
+     item.classList.toggle("remove-animation"); 
+    }
+  });
+  console.log("it is off!");
+};
